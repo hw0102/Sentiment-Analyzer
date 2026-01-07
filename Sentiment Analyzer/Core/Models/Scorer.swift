6979bc387c6c5beb34cef4a4cad32809efc4e3b0
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-01-07 17:33:16 -0500

    set up nlp to parse sentiment score one paragraph at a time

--- Sentiment Analyzer/Core/Models/Scorer.swift
+++ Sentiment Analyzer/Core/Models/Scorer.swift
@@ -5,4 +5,22 @@
 //  Created by Hello World on 1/7/26.
 //
 
-import Foundation
+import NaturalLanguage
+
+
+struct Scorer {
+    let tagger = NLTagger(tagSchemes: [.sentimentScore])
+    
+    /// return a sentiment score of a body of text
+    func score(_ text: String) -> Double {
+        var sentimentScore: Double = 0
+        tagger.string = text
+        tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .paragraph, scheme: .sentimentScore) { tag, _ in
+            if let sentimentString = tag?.rawValue, let score = Double(sentimentString) {
+                sentimentScore += score
+            }
+            return true;
+        }
+        return sentimentScore
+    }
+}
