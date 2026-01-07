3ddea8a5f9bb3938ecebcd9dad975e8ed02e505c
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-01-07 14:33:38 -0500

    Initial Commit

---
+++ Sentiment Analyzer/Sentiment_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentiment_AnalyzerApp.swift
+//  Sentiment Analyzer
+//
+//  Created by Hello World on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Sentiment_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
